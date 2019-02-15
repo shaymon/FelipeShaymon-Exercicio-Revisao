9f3a97fdeb9bfce6d8a515d733ab2d3407b1b92b
FELIPE SHAYMON SILVA DOS SANTOS <26145406@sempreunama.com.br>
2019-02-15 19:51:21 -0300

    Initial commit

---
+++ src/ex01/main1.java
@@ -0,0 +1,21 @@
+package ex01;
+
+public class main1 {
+
+    public static void main(String[] args){
+        Sacanner s  = new Scanner(System.in);
+        double conta = s.nextDouble();
+        System.out.print("Valor Consumido");
+        double valorGast;
+        double valorTot;
+
+
+
+
+
+
+    }
+
+
+
+}
