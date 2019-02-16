1da781c775acef664db10dc9b27067fa23cdbddc
shaymon <felipeshaymon9@gmail.com>
2019-02-15 21:23:33 -0300

    ex02

---
+++ src/ex02/main2.java
@@ -0,0 +1,15 @@
+package ex02;
+
+import java.util.Scanner;
+
+public class main2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+
+
+
+
+    }
+
+}
