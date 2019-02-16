94ce80b7dd2c8523152a3f40e374572e03128b4b
shaymon <felipeshaymon9@gmail.com>
2019-02-15 21:08:32 -0300

    ex01

--- src/ex01/main1.java
+++ src/ex01/main1.java
@@ -1,21 +1,23 @@
 package ex01;
 
+import java.util.Scanner;
+
 public class main1 {
 
-    public static void main(String[] args){
-        Sacanner s  = new Scanner(System.in);
-        double conta = s.nextDouble();
-        System.out.print("Valor Consumido");
-        double valorGast;
-        double valorTot;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nome do Garçom: ");
+        String nomeGarc = sc.nextLine();
+        System.out.print("Valor das Despesas: ");
+        double valorDespesa = sc.nextDouble();
+        double valorTot = valorDespesa * 1.1;
+        double valorGorjet = valorTot - valorDespesa;
 
-
-
-
+        System.out.println("Valor Total da conta: " + valorTot);
+        System.out.print("Valor da gorjeta: " + valorGorjet);
 
 
     }
-
 
 
 }
