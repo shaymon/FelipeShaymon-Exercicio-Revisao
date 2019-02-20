7dcba05c570fa837b365f1d353595af6c944fee2
shaymon <felipeshaymon9@gmail.com>
2019-02-20 16:15:29 -0300

    ex02

--- src/ex02/main2.java
+++ src/ex02/main2.java
@@ -1,13 +1,24 @@
 package ex02;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class main2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        double volume;
+        double pi=3.141559;
+        double altura;
+        double r;
+        System.out.println("Informe o raio: ");
+        r = Double.parseDouble(JOptionPane.showInputDialog("informe o raio"));
 
+        System.out.println("Informe a altura: ");
+        altura = Double.parseDouble(JOptionPane.showInputDialog("informe a altura: "));
 
+        volume = pi*(r*r)*altura;
+
+        System.out.println("O valor do volume de uma lata de óleoé " + volume);
 
 
     }
